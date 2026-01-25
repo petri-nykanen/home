efbb971f99b931e7bf6fd40df9a9de6b281d4a02
Petri Nykänen <petri.nykaenen@gmail.com>
2026-01-25 17:29:57 +0200

    Basic functions established

---
+++ typescript/src/pages/AboutMePage.tsx
@@ -0,0 +1,12 @@
+import AboutMeContent from "@/components/site-content/about-me-content";
+import React from "react";
+
+const AboutMePage = () => {
+  return (
+    <>
+      <AboutMeContent />
+    </>
+  );
+};
+
+export default AboutMePage;
---
+++ typescript/src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import LandingContent from "@/components/site-content/landing-content";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <>
+      <LandingContent />
+    </>
+  );
+};
+
+export default LandingPage;
