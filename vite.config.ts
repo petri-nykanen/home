f2a2a8d569944a6ebd6b66d6d4afbeaf1b712e91
Petri Nykänen <petri.nykaenen@gmail.com>
2026-08-09 23:30:29 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
